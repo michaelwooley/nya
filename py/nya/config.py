c89549bc1bd1d39a099a65e29abd60453a57bdda
Michael Wooley <michaelwooley@users.noreply.github.com>
2023-01-05 23:53:04 -0500

    Grab bag :shrug:  (#15)
    Does _not_ address #14

---
+++ py/nya/config.py
@@ -0,0 +1,28 @@
+"""Project config"""
+
+
+from pathlib import Path
+
+from pydantic import BaseSettings, Field, SecretStr
+
+ROOT_DIR = Path(__file__).parent.parent
+DOTENV_PATH = ROOT_DIR.joinpath(".env")
+
+
+class Settings(BaseSettings):
+    """Project config"""
+
+    root_dir: Path = Field(default=ROOT_DIR, description="Top-level of python pkg")
+
+    github_token: SecretStr = Field(
+        ...,
+        env="GITHUB_PERSONAL_ACCESS_TOKEN",
+        description="GitHub personal access token for GH api.",
+    )
+
+    class Config:
+        env_file = DOTENV_PATH
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
---
+++ py/nya/github/client.py
@@ -0,0 +1,1 @@
+"""Github API client (partial)"""
